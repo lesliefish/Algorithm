e3dc55bab6d2ed8b091e3f0fd0bff7254b952e58
leiyu <yuleics@outlook.com>
2018-10-18 13:19:11 +0800

    栈和队列基本接口实现

--- src/lei/yu/Main.java
+++ src/lei/yu/Main.java
@@ -2,11 +2,24 @@ package lei.yu;
 
 import lei.yu.algorithm.book.chapter1.BinarySearch;
 import lei.yu.algorithm.book.chapter1.CreateStringArrayBySplit;
+import lei.yu.algorithm.book.chapter1.ResizingArrayStack;
+
+import java.util.Iterator;
 
 public class Main {
 
     public static void main(String[] args) {
+        ResizingArrayStack<String> resizingArrayStack = new ResizingArrayStack<String>();
+        resizingArrayStack.push("111");
+        resizingArrayStack.push("222");
+        resizingArrayStack.push("333");
+        resizingArrayStack.push("444");
+        resizingArrayStack.push("555");
 
-        CreateStringArrayBySplit.test();
+        Iterator<String> iter = resizingArrayStack.iterator();
+        System.out.println(resizingArrayStack.size());
+        while (iter.hasNext()) {
+            System.out.println(iter.next());
+        }
     }
 }
