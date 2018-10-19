d552df649d8e3bc42cdf245fc14e224059fb57e8
leiyu <yuleics@outlook.com>
2018-10-19 13:24:15 +0800

    背包数据结构

--- src/lei/yu/Main.java
+++ src/lei/yu/Main.java
@@ -2,6 +2,7 @@ package lei.yu;
 
 import lei.yu.algorithm.book.chapter1.BinarySearch;
 import lei.yu.algorithm.book.chapter1.CreateStringArrayBySplit;
+import lei.yu.algorithm.book.chapter1.EasyBag;
 import lei.yu.algorithm.book.chapter1.ResizingArrayStack;
 
 import java.util.Iterator;
@@ -9,17 +10,6 @@ import java.util.Iterator;
 public class Main {
 
     public static void main(String[] args) {
-        ResizingArrayStack<String> resizingArrayStack = new ResizingArrayStack<String>();
-        resizingArrayStack.push("111");
-        resizingArrayStack.push("222");
-        resizingArrayStack.push("333");
-        resizingArrayStack.push("444");
-        resizingArrayStack.push("555");
 
-        Iterator<String> iter = resizingArrayStack.iterator();
-        System.out.println(resizingArrayStack.size());
-        while (iter.hasNext()) {
-            System.out.println(iter.next());
-        }
     }
 }
