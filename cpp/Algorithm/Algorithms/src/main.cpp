e05b3ec09cf594b94b16e0adb47702fcf650bce6
leiyu <yuleics@outlook.com>
2019-04-16 21:19:27 +0800

    two sum

---
+++ cpp/Algorithm/Algorithms/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "stdafx.h"
+#include "Arrays/TwoSum.h"
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    Solution::twoSum({ 1,3,5,8,9,4,6,11}, 17);
+
+    return 0;
+}
