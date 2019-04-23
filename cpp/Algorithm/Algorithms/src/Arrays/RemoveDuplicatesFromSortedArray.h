fee983371eb7bb3870b4e701ec35241e7279bc50
leiyu <yuleics@outlook.com>
2019-04-23 12:35:01 +0800

    Remove Duplicates From Sorted Array2

--- cpp/Algorithm/Algorithms/src/Arrays/RemoveDuplicatesFromSortedArray.h
+++ cpp/Algorithm/Algorithms/src/Arrays/RemoveDuplicatesFromSortedArray.h
@@ -22,16 +22,16 @@ public:
             return 0;
         }
 
-        int i = 0;
+        int index = 0;
         for (int j = 1; j < nums.size(); j++)
         {
-            if (nums[i] != nums[j])
+            if (nums[index] != nums[j])
             {
-                nums[++i] = nums[j];
+                nums[++index] = nums[j];
             }
         }
 
-        int newLen = ++i;
+        int newLen = ++index;
         nums.resize(newLen);
         return newLen;
     }
--- cpp/Algorithm/Algorithms/src/main.cpp
+++ cpp/Algorithm/Algorithms/src/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include "stdafx.h"
-#include "Arrays/RemoveDuplicatesFromSortedArray.h"
+#include "Arrays/RemoveDuplicatesFromSortedArray2.h"
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-    RemoveDuplicatesFromSortedArray::test();
+    RemoveDuplicatesFromSortedArray2::test();
 
     return 0;
 }
