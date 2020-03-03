a6c2aaa69f67c8ee5a1ff9d1867ef07a7304e08e
leiyu <yuleics@outlook.com>
2020-03-03 15:41:12 +0800

    update

---
+++ Coder/华为机试_字符统计/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	string inputString;
+
+	while (getline(cin, inputString))
+	{
+		map<char, int> myMap{};
+		for (int i = 0; i < inputString.size(); ++i)
+		{
+			char ch = inputString[i];
+			if (isalpha(ch) || isdigit(ch) || ch == ' ')
+			{
+				myMap[ch]++;
+			}
+		}
+
+		map<int, vector<char>> outMap{};
+		for (auto iter : myMap)
+		{
+			outMap[iter.second].emplace_back(iter.first);
+		}
+
+		for (auto riter = outMap.rbegin(); riter != outMap.rend(); ++riter)
+		{
+			for (auto it : riter->second)
+			{
+				cout << it;
+			}
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
